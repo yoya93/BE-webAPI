14aae476e6fab818d52e246b5416e6076b27fa6e
Jorge <jorgelopezgonzalez474@gmail.com>
2021-06-11 14:27:29 -0400

    Agregar archivos de proyecto.

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Entidades;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("v1/api/usuarios")]
+    public class UsuariosController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<List<Usuario>> Get()
+        {
+            return new List<Usuario>()
+            {
+                new Usuario() {id =1 , nombre = "Jorge" },
+                new Usuario() {id =2 , nombre = "Juan"}
+            };
+        }
+    }
+}
---
+++ Entidades/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Entidades
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+    }
+}
